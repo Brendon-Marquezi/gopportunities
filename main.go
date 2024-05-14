364d5501e6637fb1189d0c65e7db4244f103fbb8
Brendon Marquezi <brendon.marquezi@jala.university>
2024-05-14 02:55:30 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/Brendon-Marquezi/gopportunities/router"
+import (
+	"github.com/Brendon-Marquezi/gopportunities/config"
+	"github.com/Brendon-Marquezi/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// initialize router
 	router.Initialize()
